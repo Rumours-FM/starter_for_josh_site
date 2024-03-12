80d4eef1cc0261437d035e79880064b2f1f45e41
Rumours-FM <jaydentier@gmail.com>
2024-03-12 18:07:51 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,22 @@
+    <footer class="container-fluid"> <!-- div for main image to stretch to edges -->
+        
+        <div class="container"> <!-- div to hold content in the middle -->
+
+            <div class="row text-center">
+
+                <div class="col-md-6">
+                    <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange">WHITKIN</span></a>
+                </div>
+            
+                <div class="col-md-6">
+                    <p>Terms and Conditions</p>
+                </div>
+
+            </div> <!-- row -->
+
+        </div> <!-- container -->
+
+    </footer> <!-- container-fluid -->
+    <?php wp_footer(); ?>
+</body>
+</html>
