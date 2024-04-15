b7be7bfff46fba847de3942e605578354deda67b
Rumours-FM <jaydentier@gmail.com>
2024-04-15 19:44:28 +0800

    Week8 Updates

--- footer.php
+++ footer.php
@@ -5,11 +5,17 @@
             <div class="row text-center">
 
                 <div class="col-md-6">
-                    <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange">WHITKIN</span></a>
+                    <!-- <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange">WHITKIN</span></a> -->
+                    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-left') ) : 
+                        endif; ?>
+
                 </div>
             
                 <div class="col-md-6">
-                    <p>Terms and Conditions</p>
+                    <!-- <p>Terms and Conditions</p> -->
+                    <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-right') ) : 
+                    endif; ?>
+
                 </div>
 
             </div> <!-- row -->
